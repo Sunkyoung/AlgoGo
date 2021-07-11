fecd5befc5f4c40d5549654c84b329ea351fd6e2
Sunkyoung <sunk10926@gmail.com>
2021-07-11 15:26:10 +0900

    feat: add solution code in lecture

--- Code/이코테2021/Greedy/Chap11/guild_adventure.py
+++ Code/이코테2021/Greedy/Chap11/guild_adventure.py
@@ -14,3 +14,18 @@ while True:
     count += 1
 
 print(count)
+
+# cf) 솔루션 코드
+# n = int(input())
+# data = list(map(int, input().split()))
+# data.sort()
+
+# result = 0
+# count = 0
+
+# for i in data:
+#     count += 1
+#     if count >= i:
+#         result += i
+#         count = 0
+# print(result)
