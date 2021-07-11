f961dcc17156c2c3266af8fac9dc1e2dc6217a7c
Sunkyoung <sunk10926@gmail.com>
2021-07-11 16:22:57 +0900

    fix: 1 is better to add rather than multiplication

--- Code/이코테2021/Greedy/Chap11/mult_or_sum.py
+++ Code/이코테2021/Greedy/Chap11/mult_or_sum.py
@@ -2,6 +2,6 @@ S = input()
 result = int(S[0])
 for s in S[1:]:
     num = int(s)
-    result = result + num if result == 0 or num == 0 else result * num
+    result = result + num if result <= 1 or num <= 1 else result * num
 
 print(result)
